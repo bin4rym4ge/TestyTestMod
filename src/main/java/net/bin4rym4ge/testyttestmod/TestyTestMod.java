c93803dee5b08bc5c594ff73cd50d8bfaacce5fd
bin4rym4ge <none>
2023-07-17 12:41:40 -0700

    ModItems
    Registry.register(Registries.ITEM, new Identifier(TestyTestMod.MOD_ID,
    name), item)
    
    "net.minecraft.registry.Registries" did not show up. Had to enter
    manually to resolve Registries.ITEM.

--- src/main/java/net/bin4rym4ge/testyttestmod/TestyTestMod.java
+++ src/main/java/net/bin4rym4ge/testyttestmod/TestyTestMod.java
@@ -3,6 +3,7 @@ package net.bin4rym4ge.testyttestmod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.bin4rym4ge.testyttestmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class TestyTestMod implements ModInitializer {
@@ -14,6 +15,8 @@ public class TestyTestMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		
+		ModItems.registerModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
--- src/main/java/net/bin4rym4ge/testyttestmod/item/ModItems.java
+++ src/main/java/net/bin4rym4ge/testyttestmod/item/ModItems.java
@@ -1,5 +1,25 @@
 package net.bin4rym4ge.testyttestmod.item;
 
+import net.bin4rym4ge.testyttestmod.TestyTestMod;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
+	
+	private static Item registerItem(String name, Item item) {
+		
+		return Registry.register(Registries.ITEM, new Identifier(TestyTestMod.MOD_ID, name), item)
+		
+	}
+	
+	public static void registerModItems() {
+		
+		TestyTestMod.LOGGER.info("Registering mod items for" + TestyTestMod.MOD_ID);
+		
+		
+		
+	}
 
 }
