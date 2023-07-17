bd05c3900fb3a8532ad404598480efed69c8ccd2
bin4rym4ge <none>
2023-07-17 09:07:21 -0700

    Init

---
+++ src/main/java/net/bin4rym4ge/testyttestmod/TestyTestMod.java
@@ -0,0 +1,20 @@
+package net.bin4rym4ge.testyttestmod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TestyTestMod implements ModInitializer {
+	
+	
+	public static final String MOD_ID = "testyttestmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/bin4rym4ge/testyttestmod/TestyTestModClient.java
@@ -0,0 +1,13 @@
+package net.bin4rym4ge.testyttestmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestyTestModClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
