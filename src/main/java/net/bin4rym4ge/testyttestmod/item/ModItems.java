b304bf84532e702f21d3dc0b0191fd6285ff2dba
bin4rym4ge <none>
2023-07-17 13:18:50 -0700

    ModItems

--- src/main/java/net/bin4rym4ge/testyttestmod/item/ModItems.java
+++ src/main/java/net/bin4rym4ge/testyttestmod/item/ModItems.java
@@ -1,16 +1,26 @@
 package net.bin4rym4ge.testyttestmod.item;
 
 import net.bin4rym4ge.testyttestmod.TestyTestMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 	
+	public static final Item TESTYTESTITEM = registerItem("testy_test_item", new Item(new FabricItemSettings()));
+	
+	private static void addItemsToIngerdientTabItemGoup(FabricItemGroupEntries entries) {
+		entries.add(TESTYTESTITEM);
+	}
+	
 	private static Item registerItem(String name, Item item) {
 		
-		return Registry.register(Registries.ITEM, new Identifier(TestyTestMod.MOD_ID, name), item)
+		return Registry.register(Registries.ITEM, new Identifier(TestyTestMod.MOD_ID, name), item);
 		
 	}
 	
@@ -18,7 +28,7 @@ public class ModItems {
 		
 		TestyTestMod.LOGGER.info("Registering mod items for" + TestyTestMod.MOD_ID);
 		
-		
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngerdientTabItemGoup);
 		
 	}
 
