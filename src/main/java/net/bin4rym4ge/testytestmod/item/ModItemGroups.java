53717d121c491f7103b8da53063e1e66261849b2
bin4rym4ge <none>
2023-07-17 17:21:31 -0700

    Added items to test group

--- src/main/java/net/bin4rym4ge/testytestmod/item/ModItemGroups.java
+++ src/main/java/net/bin4rym4ge/testytestmod/item/ModItemGroups.java
@@ -4,6 +4,7 @@ import net.bin4rym4ge.testytestmod.TestyTestMod;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -15,6 +16,11 @@ public class ModItemGroups {
 			new Identifier(TestyTestMod.MOD_ID, "testytestgroup"), 
 			FabricItemGroup.builder().displayName(Text.translatable("itemgroup.testytest"))
 			.icon(() -> new ItemStack(ModItems.TESTYTESTITEM)).entries((DisplayContext, entries) -> {
+				
+				entries.add(ModItems.TESTYTESTITEM);
+				entries.add(ModItems.TESTIERTESTITEM);
+				
+				entries.add(Items.REDSTONE);
 				
 			}).build());
 	
